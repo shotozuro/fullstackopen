fbab976fb793c823251aebf3603ff7784e6c1db2
Shalahuddin Fajri <shotozuro@gmail.com>
2019-08-27 06:01:53 +0700

    step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,7 +5,17 @@ const Header = (props) => (
   <h1>{props.course}</h1>
 )
 
-const Content = (props) => null
+const Content = (props) => (
+  <div>
+    {
+      props.parts.map(part => <Part name={part.name} exercises={part.exercises} />)
+    }
+  </div>
+)
+
+const Part = (props) => (
+  <p>{props.name} {props.exercises}</p>
+)
 
 const Total = (props) => (
   <p>Number of exercises {props.total} </p>
@@ -13,18 +23,16 @@ const Total = (props) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} />
+      <Total total={parts.reduce((prev, curr) => (prev + curr))} />
     </div>
   )
 }
