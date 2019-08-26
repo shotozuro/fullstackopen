28223024dc0b441142b9d4e632b7c8bf3be7dc98
Shalahuddin Fajri <shotozuro@gmail.com>
2019-08-27 05:54:46 +0700

    step1

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => (
+  <h1>{props.course}</h1>
+)
+
+const Content = (props) => null
+
+const Total = (props) => (
+  <p>Number of exercises {props.total} </p>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+ReactDOM.render(<App />, document.getElementById('root'))
