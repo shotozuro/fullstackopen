936ea468677e29413e76ec362c76e8b2e05bd2c5
Shalahuddin Fajri <shotozuro@gmail.com>
2019-08-27 22:12:08 +0700

    2.10: The Phonebook Step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,39 @@
 import React, { useState } from 'react'
 
+const Filter = ({ query, onChange }) => {
+  return (
+    <div>filter shown with: <input value={query} onChange={onChange} /></div>
+  )
+}
+
+const PersonForm = (props) => {
+  const { newName, newPhone, onNewNameChange, onNewPhoneChange, onSubmit } = props
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+      name: <input value={newName} onChange={onNewNameChange} />
+      </div>
+      <div>
+      number: <input value={newPhone} onChange={onNewPhoneChange} />
+      </div>
+      <div>
+        <button type='submit'>add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons, query }) => {
+  return persons
+    .filter(person => person.name.toLowerCase().includes(query.toLowerCase()))
+    .map(person => <Person key={person.name} person={person} />)
+}
+
+const Person = ({ person }) => {
+  return (
+    <p>{person.name} {person.phone}</p>
+  )
+}
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas', phone: '040-1234567' }
@@ -31,37 +65,21 @@ const App = () => {
   const handleNewFilterChange = (event) => {
     setNewNameFilter(event.target.value)
   }
-
   return (
     <div>
-      <h1>Phonebook</h1>
-      <div>filter shown with: <input value={newNameFilter} onChange={handleNewFilterChange} /></div>
-      <h2>Add New</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNewNameChange} />
-        </div>
-        <div>
-          number: <input value={newPhone} onChange={handleNewPhoneChange} />
-        </div>
-        <div>
-          <button type='submit'>add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {
-        newNameFilter.length > 0
-          ? persons
-            .filter(person => person.name.includes(newNameFilter))
-            .map(person =>
-              <p key={person.name}>
-                {person.name} {person.phone}
-              </p>)
-          : persons.map(person =>
-            <p key={person.name}>
-              {person.name} {person.phone}
-            </p>)
-      }
+      <h2>Phonebook</h2>
+      <Filter query={newNameFilter} onChange={handleNewFilterChange} />
+
+      <h3>Add New</h3>
+      <PersonForm
+        newName={newName}
+        newPhone={newPhone}
+        onNewNameChange={handleNewNameChange}
+        onNewPhoneChange={handleNewPhoneChange}
+        onSubmit={handleSubmit} />
+
+      <h3>Numbers</h3>
+      <Persons persons={persons} query={newNameFilter} />
     </div>
   )
 }
